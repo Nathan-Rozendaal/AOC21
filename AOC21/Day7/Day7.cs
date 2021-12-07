cac132e487915547fc27b023f0b77a8a212ca9dc
Nathan Rozendaal <nathan.rozendaal@gmail.com>
2021-12-07 08:37:24 +0100

    Finished day 7!

---
+++ AOC21/Day7/Day7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC21
+{
+	class Day7
+	{
+		List<int> data;
+		public Day7()
+		{
+			string[] lines = System.IO.File.ReadAllLines(@"../../../Day7/input.txt");
+			data = Array.ConvertAll(lines[0].Split(','), s => int.Parse(s)).ToList();
+		}
+
+		public void run()
+		{
+			// part one
+			data.Sort();
+			int median = data[data.Count / 2];
+			int fuel = 0;
+			foreach (var item in data)
+			{
+				fuel += Math.Abs(item - median);
+			}
+			Console.WriteLine(fuel);
+
+			//part two
+			int avarage = (int)data.Average();
+			fuel = 0;
+			foreach (var item in data)
+			{
+				for (int i = 1; i <= Math.Abs(item - avarage); i++)
+				{
+					fuel += i;
+				}
+			}
+			Console.WriteLine(fuel);
+		}
+	}
+}
--- AOC21/Program.cs
+++ AOC21/Program.cs
@@ -6,7 +6,7 @@ namespace AOC21
 	{
 		static void Main(string[] args)
 		{
-			var day = new Day6();
+			var day = new Day7();
 			day.run();
 		}
 	}
